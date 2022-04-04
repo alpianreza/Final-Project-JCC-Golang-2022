97256be7e62f9c10a852275e1839f633711059a6
alpianreza <alpianrezha@gmail.com>
2022-04-04 11:59:22 +0700

    fix database

--- models/blog.go
+++ models/blog.go
@@ -8,21 +8,21 @@ import (
 
 type (
 	Posts struct {
-		ID          int        `gorm:"primary_key" json:"id"`
+		ID          int        `gorm:"primary_key,column:id" json:"id"`
 		PostTitle   string     `gorm:"not null,colomn:title" json:"post_title"`
 		PostContent string     `gorm:"not null" json:"post_content"`
 		Publish     bool       `gorm:"not null" json:"publish"`
-		CreatedAt   time.Time  `json:"created_at"`
-		UpdatedAt   time.Time  `json:"updated_at"`
-		UserID      uint       `gorm:"not null" json:"user_id"`
+		CreatedAt   time.Time  `gorm:"column:create_at" json:"created_at"`
+		UpdatedAt   time.Time  `gorm:"column:updated_at" json:"updated_at"`
+		UserID      uint       `gorm:"references:author_id" json:"user_id"`
 		CategoryID  int        `gorm:"not null" json:"category_id"`
-		Users       []Users    `gorm:"foreignKey:id;references:UserID" "json:"users"`
+		Users       Users      `gorm:"foreignKey:user_id;" json:"users_id"`
 		Category    []Category `gorm:"foreignKey:category_id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"category"`
 		Comments    []Comments `gorm:"many2many:post_comments;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"comments"`
 		Meta        []PostMeta `gorm:"foreignKey:post_id;" json:"meta"`
 	}
 	PostMeta struct {
-		MetaID    int    `gorm:"primaryKey,index" json:"meta_id"`
+		MetaID    int    `json:"meta_id"`
 		PostID    int    `json:"post_id"`
 		MetaKey   string `json:"meta_key"`
 		MetaValue string `json:"meta_value"`
