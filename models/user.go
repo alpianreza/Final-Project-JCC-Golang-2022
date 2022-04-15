1f779d8a691809d54f65a29acf4ce22b2d0cb748
alpianreza <alpianrezha@gmail.com>
2022-04-15 12:52:39 +0700

    update

--- models/user.go
+++ models/user.go
@@ -22,7 +22,7 @@ type (
 		Role      string    `json:"role"`
 		CreatedAt time.Time `json:"created_at"`
 		UpdatedAt time.Time `json:"updated_at"`
-		Posts     *[]Post   `json:"posts" gorm:"foreignKey:user_id"`
+		Posts     *[]Posts  `json:"posts" gorm:"foreignKey:user_id"`
 	}
 
 	Guest struct {
